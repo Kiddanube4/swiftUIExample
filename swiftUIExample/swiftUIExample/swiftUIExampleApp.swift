1771136cd14b3f9ffaa3137c79545a7b120282da
Kiddanube4 <89117767+Kiddanube4@users.noreply.github.com>
2023-02-01 12:08:30 +0300

    app init

---
+++ swiftUIExample/swiftUIExample/swiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIExampleApp.swift
+//  swiftUIExample
+//
+//  Created by Namik Karabiyik on 1.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
